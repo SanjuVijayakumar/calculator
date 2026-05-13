8c5348002ed83bf1810001574cd92dcde935f498
SanjuVijayakumar <33987375+SanjuVijayakumar@users.noreply.github.com>
2026-05-13 09:27:05 +0530

    Delete CharacterCounter directory

--- CharacterCounter/script.js
+++
@@ -1,8 +0,0 @@
-function updateCount() {
-    var text = document.getElementById("text").value;
-    var count = text.length;
-    document.getElementById("count").innerText = count;
-}
-
-document.getElementById("text").addEventListener("input", updateCount);
-updateCount();
